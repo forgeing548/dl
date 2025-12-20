b7800b9b7667ac4710e8c3a874b499e244db1218
forgeing548 <Crockbird237@gmail.com>
2025-12-20 19:29:21 +0400

    Update StreetlifeRemasteredakls.lua

--- StreetlifeRemasteredakls.lua
+++ StreetlifeRemasteredakls.lua
@@ -3,5 +3,5 @@ local UserInputService = game:GetService("UserInputService")
 if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
     loadstring(game:HttpGet("MOBILE_LOADSTRING_URL_HERE"))()
 else
-    loadstring(game:HttpGet("PC_LOADSTRING_URL_HERE"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/forgeing548/dl/refs/heads/main/Streetlifepc.lua"))()
 end
